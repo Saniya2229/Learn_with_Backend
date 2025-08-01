6da16a75d2d57d1713248f7b343ae948731ec0b8
Saniya2229 <saniyahakim22@gmail.com>
2025-08-01 17:22:51 +0530

    add important note

--- server-node.js
+++ server-node.js
@@ -22,3 +22,15 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server is listening at http://${hostname}:${port}/`);
 });
+
+/*
+when you run this code also you have application name was postman
+you also required to install from your system.
+once you can install all the requirements you can just 
+1.run this code 
+2.copy the url like "https:// 127.0.0.1" 
+3.go to postman app 
+4.click the new button 
+5.select the "http"
+6.paste it here.
+*/
