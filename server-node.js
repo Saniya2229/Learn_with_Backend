3a377e364d8697089157a5a4c1386fa207b8094d
Saniya2229 <saniyahakim22@gmail.com>
2025-08-01 16:10:18 +0530

    create own node server

---
+++ server-node.js
@@ -0,0 +1,24 @@
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello Users");
+  } else if (req.url === "/users") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Thanks for visiting the users page");
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("404 Not found");
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server is listening at http://${hostname}:${port}/`);
+});
